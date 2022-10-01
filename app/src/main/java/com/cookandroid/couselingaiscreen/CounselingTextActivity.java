177b55f7c47403adcef4891d6909bb0721ab734e
정다운 <emforhs4686@naver.com>
2022-10-02 02:29:54 +0900

    flasktest

--- app/src/main/java/com/cookandroid/couselingaiscreen/CounselingTextActivity.java
+++ app/src/main/java/com/cookandroid/couselingaiscreen/CounselingTextActivity.java
@@ -1,16 +1,103 @@
 package com.cookandroid.couselingaiscreen;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class CounselingTextActivity extends AppCompatActivity {
 
+    private static final String urls = "http://127.0.0.1:5000/";
+    private EditText input_chat;
+    Button sendText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.counselingtextscreen);
+
+        input_chat=findViewById(R.id.chatting);
+        sendText=findViewById(R.id.send);
+
+        sendText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendServer();
+            }
+        });
+    }
+
+    public void sendServer(){
+        class sendData extends AsyncTask<Void, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+            }
+
+            @Override
+            protected void onProgressUpdate(Void... values) {
+                super.onProgressUpdate(values);
+            }
+
+            @Override
+            protected void onCancelled(String s) {
+                super.onCancelled(s);
+            }
+
+            @Override
+            protected void onCancelled() {
+                super.onCancelled();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids){
+                try{
+                    OkHttpClient client = new OkHttpClient();  // okHttpClient 호출
+                    JSONObject jsonInput = new JSONObject(); // Json객체 생성
+                    jsonInput.put("userChat", input_chat.getText().toString());
+
+                    RequestBody reqBody = RequestBody.create(
+                            MediaType.parse("application/json; charset=utf-8"),
+                            jsonInput.toString()
+                    );
+
+                    Request request = new Request.Builder()
+                            .post(reqBody)
+                            .url(urls)
+                            .build();
+
+                    Response responses = null;
+                    responses = client.newCall(request).execute();
+                    System.out.println(responses.body().string());
+                } catch (JSONException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
+        sendData sendData = new sendData();
+        sendData.execute();
     }
 }

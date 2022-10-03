55571d8c1ebccba4464875ee77de7db1f604c3b4
임세진 <sejintong@naver.com>
2022-10-04 05:23:19 +0900

    flask 챗봇 연결

--- server.py
+++ server.py
@@ -1,22 +1,44 @@
-import flask
-import requests
+from flask import Flask, jsonify
+from flask_restful import Api
 
-app = flask.Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
+Q_TKN = "<usr>"
+A_TKN = "<sys>"
+BOS = '</s>'
+EOS = '</s>'
+MASK = '<unused0>'
+SENT = '<unused1>'
+PAD = '<pad>'
 
-#연결 상태 표시
-@app.route('/', methods=['GET'])
-def handle_call():
-    return "Successfully Connected"
+save_ckpt_path = f"./kogpt2.pth" # 경로 설정
 
-#get method
-@app.route('/getfact', methods=['GET'])
-def get_fact():
-    return "Hey!! I'm the fact you got!!!"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # 모델 gpu 사용 여부
 
-#post method
-@app.route('/getname/<name>', methods=['POST'])
-def extract_name(name):
-    return "I got your name" + name;
+model = torch.load(save_ckpt_path, map_location=device) # 저장한 모델 불러오기
+# 모델 구조 불러오기
+
+#학습한 모델 불러오기
+model.eval()
+
+sent = '0'
+
+app = flask.Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌
+
+api = Api(app)
+
+# 서버로 입력 들어온 텍스트 토큰화 시키기 위한 토크나이저
+koGPT2_TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
+            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
+            pad_token=PAD, mask_token=MASK)
+
+@app.route('/echo_call/<param>') #get echo api
+def get_echo_call(param): #param = 입력으로 들어오는 텍스트
+    input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + param + SENT + sent + A_TKN)).unsqueeze(dim=0)
+    pred = model(input_ids)
+    pred = pred.logits
+    gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
+    gen = gen[:gen.find(".")]
+
+    return jsonify({"param": gen})
 
 if __name__ == "__main__":
     app.run("192.168.123.105", port=5000, debug=True)

cf3121c566d26af44af644dd5240d5d464a62024
임세진 <sejintong@naver.com>
2022-10-08 04:05:54 +0900

    flask 챗봇 서버 연결 수정

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
-from flask_restful import Api
-from transformers import PreTrainedTokenizerFast
+from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
 import torch
-
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.core.lightning import LightningModule
+from torch.utils.data import DataLoader, Dataset
+from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
+from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
+import re
+from pathlib import Path
 
 Q_TKN = "<usr>"
 A_TKN = "<sys>"
@@ -12,36 +19,121 @@ MASK = '<unused0>'
 SENT = '<unused1>'
 PAD = '<pad>'
 
-save_ckpt_path = f"./kogpt2.pth" # 경로 설정
+koGPT2_TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
+            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
+            pad_token=PAD, mask_token=MASK)
+model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # 모델 gpu 사용 여부
+data_path = Path(__file__).parent.resolve().joinpath("./data/Chatbot_Data.csv")
+Chatbot_Data = pd.read_csv(data_path, encoding='utf-8')
 
-model = torch.load(save_ckpt_path, map_location=device) # 저장한 모델 불러오기
-# 모델 구조 불러오기
+class ChatbotDataset(Dataset):
+    def __init__(self, chats, max_len=40):
+        self._data = chats
+        self.max_len = max_len
+        self.q_token = Q_TKN
+        self.a_token = A_TKN
+        self.sent_token = SENT
+        self.eos = EOS
+        self.mask = MASK
+        self.tokenizer = koGPT2_TOKENIZER
 
-#학습한 모델 불러오기
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, idx):
+        turn = self._data.iloc[idx]
+        q = turn["Q"]
+        q = re.sub(r"([?.!,])", r" ", q)
+
+        a = turn["A"]
+        a = re.sub(r"([?.!,])", r" ", a)
+
+        q_toked = self.tokenizer.tokenize(self.q_token + q + self.sent_token)
+        q_len = len(q_toked)
+
+        a_toked = self.tokenizer.tokenize(self.a_token + a + self.eos)
+        a_len = len(a_toked)
+
+
+        if q_len > self.max_len:
+            a_len = self.max_len - q_len
+            if a_len <= 0:
+                q_toked = q_toked[-(int(self.max_len / 2)) :]
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+
+
+        if q_len + a_len > self.max_len:
+            a_len = self.max_len - q_len
+            if a_len <= 0:
+                q_toked = q_toked[-(int(self.max_len / 2)) :]
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+
+        labels = [self.mask,] * q_len + a_toked[1:]
+
+        mask = [0] * q_len + [1] * a_len + [0] * (self.max_len - q_len - a_len)
+
+        labels_ids = self.tokenizer.convert_tokens_to_ids(labels)
+
+        while len(labels_ids) < self.max_len:
+            labels_ids += [self.tokenizer.pad_token_id]
+
+
+        token_ids = self.tokenizer.convert_tokens_to_ids(q_toked + a_toked)
+
+        while len(token_ids) < self.max_len:
+            token_ids += [self.tokenizer.pad_token_id]
+
+
+        return (token_ids, np.array(mask), labels_ids)
+
+def collate_batch(batch):
+    data = [item[0] for item in batch]
+    mask = [item[1] for item in batch]
+    label = [item[2] for item in batch]
+    return torch.LongTensor(data), torch.LongTensor(mask), torch.LongTensor(label)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_set = ChatbotDataset(Chatbot_Data, max_len=40)
+
+train_dataloader = DataLoader(train_set, batch_size=32, num_workers=0, shuffle=True, collate_fn=collate_batch,)
+
+model.to(device)
+model.train()
+
+save_ckpt_path = Path(__file__).parent.resolve().joinpath("./checkpoint/kogpt2.pth")
+
+model = torch.load(save_ckpt_path, map_location=device)
 model.eval()
 
 sent = '0'
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌
 
-api = Api(app)
+@app.route('/', methods=['POST'])
+def index():
+    req_data = request.get_json()
+    q = req_data['message'].strip()
 
-# 서버로 입력 들어온 텍스트 토큰화 시키기 위한 토크나이저
-koGPT2_TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
-            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
-            pad_token=PAD, mask_token=MASK)
-
-@app.route('/echo_call/<param>') #get echo api
-def get_echo_call(param): #param = 입력으로 들어오는 텍스트
-    input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + param + SENT + sent + A_TKN)).unsqueeze(dim=0)
-    pred = model(input_ids)
-    pred = pred.logits
-    gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
-    gen = gen[:gen.find(".")]
-
-    return jsonify({"param": gen})
+    with torch.no_grad():
+        a = ""
+        while 1:
+            input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + q + SENT + sent + A_TKN + a)).unsqueeze(
+                dim=0)
+            pred = model(input_ids)
+            pred = pred.logits
+            gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
+            if gen == EOS:
+                break
+            a += gen.replace("▁", " ")
+        response_string = "{}".format(a.strip()) #출력문 어디에 저장되는지 확인 필요함
+        return jsonify(message=response_string)
 
 if __name__ == "__main__":
     app.run("192.168.219.105", port=5000, debug=True)

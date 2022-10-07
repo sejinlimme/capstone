fcc8d459cd27a84755b20458f2e5dbcd966fc7c7
정다운 <emforhs4686@naver.com>
2022-10-08 02:39:38 +0900

    채팅화면 수정

--- app/src/main/java/com/cookandroid/couselingaiscreen/CounselingTextActivity.java
+++ app/src/main/java/com/cookandroid/couselingaiscreen/CounselingTextActivity.java
@@ -1,72 +1,133 @@
 package com.cookandroid.couselingaiscreen;
 
+import androidx.appcompat.app.AppCompatActivity; //안드로이드의 하위버전을 지원하는 Activity
+
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;     //여러개의 Window를 가질 수 있고 이러한 Window들은 WindowManager가 관리를 합니다.
+import android.widget.AbsListView;      //어댑터(adapter)라는 객체를 갖는다는 점입니다. 어댑터는 어댑터 뷰와 자식 뷰들 사이를 이어주는 중간 역할을 합니다.
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;  //Request: 요청객체
+import com.android.volley.RequestQueue;
+import com.android.volley.Response; //응답 (지정된 유형(예: 문자열, 이미지 또는 JSON)에 따라 전달할 파싱된 응답을 캡슐화합니다.)
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import org.json.JSONException;
+//예외구문, 문법적인 구조가 맞지 않을때, 제기슨 구성 이름이 null인경우,반환하는 메소드에서 변환할수 없는 문자열 있는 경우
+//상수를 다른 타입에 사용하는 경우, 제이슨 배열에서 사용하는 매개변수인 인텍스가 범위를 벗어난 경우, 제이슨 객체가 없는 경우
+import org.json.JSONObject;  //데이터를 구조적으로 표현하기 위한 일종의 포맷(Format)으로 네트워크를 통해 데이터를 주고받을 때 사용되는 경량의 데이터 형식을 의미합니다.
+
+import java.util.HashMap;   //많은 양의 데이터를 저장
 
 public class CounselingTextActivity extends AppCompatActivity {
 
-    private static final String baseURL = "http://192.168.123.105:5000";
-    private EditText InputChat;
-    private Button SendText, GetBtn;
-    private TextView GetInfom;
+    String message;
+    ListView list; //어댑터가 데이터를 가공해서 제공하면 어댑터 뷰는 그 데이터를 출력하는 역할
+    EditText messageText;
+    Button submitButton;
+    TextView reply;
+    String url = "http://192.168.1.7:5000/";
 
-    Retrofit mRetrofit = new Retrofit.Builder()
-            .baseUrl(baseURL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+    HashMap data = new HashMap();
 
-    CounselingTextAPI counselingTextAPI = mRetrofit.create(CounselingTextAPI.class);
+    RequestQueue mRequestQueue; //요청큐로 알아서 웹서버에 요청하고 응답까지 받아서 준다.
+    StringRequest stringRequest;
+    String rep;
+
+    String TAG = CounselingTextActivityAdapter.class.getName();
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.counselingtextscreen);
+        setContentView(R.layout.activity_main);
+        //setContent View:XML에 정의된 각 위젯들을 정의된 속성을 지정하고 상하관계에 맞춘 뒤 메모리에 올려야 합니다. 이러한 일련의 작업을 소스상에서 제공하는 게 setContentView() 함수입니다.
+        //키보드가 뷰를 밀어올리는 것을 방지
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
-        InputChat = findViewById(R.id.chatting);
-        SendText = findViewById(R.id.send);
-        GetInfom = findViewById(R.id.getInform);
-        GetBtn = findViewById(R.id.getBtn);
 
-        SendText.setOnClickListener(new View.OnClickListener() {
+        final CounselingTextActivityAdapter arrayAdapter = new CounselingTextActivityAdapter(getApplicationContext(), R.layout.counselingtextscreen_send); //final 지연변수 상수화
+        final ListView listView = (ListView) findViewById(R.id.chat_history);
+        listView.setAdapter(arrayAdapter);  //View를 드래그하여 화면에 올리면 그 위에서 다양한 데이터를 올릴 수 있다. 데이터를 올리는 부분에서 뷰와 데이터를 연결해주는 다리역할을 하는것이 어댑터(Adapter) 이다.
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL); //새로운 item 이 add 되었을 때 focus 이동을 할지를 결정하는 속성이다.
+        //listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+
+        //Observer (옵저버) 는 '관측자' 혹은 '감시자' 라는 뜻이다. 말 그대로 무언가를 감시하는 역할]]]
+        /*
+        arrayAdapter.registerDataSetObserver(new DataSetObserver() {  //registerDataSetObserver(DataSetObserver observer)를 통해서 observer를 Adapter에 연결해야 한다.
             @Override
-            public void onClick(View view) {
-                String text = InputChat.getText().toString();
-                textCallback(text);
+            public void onChanged() {  //메서드를 정의하는 Observer 객체를 만듭니다.
+                super.onChanged();  //부모클래스의 필드값이나 메소드를 직접 부를 때 붙여넣는 겁니다
+                listView.setSelection(arrayAdapter.getCount() - 1); //ListView row의 원하는 위치에 화면을 둘수가 있다.
+            }
+        });*/
+
+        messageText = (EditText) findViewById(R.id.chatting);
+        submitButton = (Button) findViewById(R.id.send);
+
+        submitButton.setOnClickListener(new View.OnClickListener() { //이벤트 리스너를 등록하고 있습니다.
+            @Override
+            public void onClick(View v) { //layout설정 xml에 추가 되어 View에서 클릭 되었을 떄 정의된 이름의 함수를 호출하여 작동 시킨다.
+                message = messageText.getText().toString();  // getText()함수는 String 클래스 타입을 리턴하지 않고, Editable 인터페이스 타입을 리턴합니다. 그래서 String 타입으로 텍스트를 사용하고자 한다면, Editable 인터페이스가 제공하는 toString() 함수를 호출하여 String 타입으로 변환해야 합니다.
+
+                sendMessage(arrayAdapter, message);
+
+                data.put("message",message);
+//                messageText.setText("");
+                postData(data, arrayAdapter); //webview에 post로 데이터 넘기는 방법
+                messageText.setText(""); //Text Object에 텍스트를 선언 하는 메서드 입니다.
             }
         });
     }
 
-    public void textCallback(String chatText) {
-        counselingTextAPI.getAIReply(chatText).enqueue(new Callback<CounselingTextDTO>() {
-            @Override
-            public void onResponse(Call<CounselingTextDTO> call, Response<CounselingTextDTO> response) {
-                //서버에서 데이터 요청 성공시
-                CounselingTextDTO result = response.body();
-                Log.d("testt", "결과는 ${result}");
+    public void postData(HashMap data, final CounselingTextActivityAdapter adapter){
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //(JsonRequest)JsonArrayRequest,JsonObjectRequest URL을 지정하고 응답에 따라 JSON 객체 또는 배열을 각각 가져옵니다.
+
+        JsonObjectRequest jsonobj = new JsonObjectRequest(Request.Method.POST, url,new JSONObject(data),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) { //respone:서버로부터 응답받을때 사용함(간략한 코드로 서버로 부터 데이터를 받아 올 수 있습니다.)
+
+                        try {
+                            final String s = response.getString("message"); // getString: receiveMessage 안에 있는 String 값을 구 할 수가 있다.
+                            receiveMessage(adapter, s);
+                        } catch (JSONException e) {
+                            e.printStackTrace(); //에러 메세지의 발생 근원지를 찾아서 단계별로 에러를 출력
+                        }
+                    }},
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i(TAG, "Error: " + error.toString());   //오류 로그 보는 줄
+                    }
+                }
+        );
+
+        mRequestQueue.add(jsonobj);
+
+    }
+
+    private boolean receiveMessage(CounselingTextActivityAdapter adapter, String msg) {
+        adapter.add(new ResponseMessage(msg, false));
+        return true;
+    }
 
 
-            }
-
-            @Override
-            public void onFailure(Call<CounselingTextDTO> call, Throwable t) {
-                //서버 요청 실패
-                t.printStackTrace();
-                Log.d("testt", "에러입니다. ${t.message}");
-            }
-        });
+    private boolean sendMessage(CounselingTextActivityAdapter adapter, String msg) {
+        adapter.add(new ResponseMessage(msg, true));
+        return true;
     }
 }
---
+++ app/src/main/java/com/cookandroid/couselingaiscreen/CounselingTextActivityAdapter.java
@@ -0,0 +1,63 @@
+package com.cookandroid.couselingaiscreen;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CounselingTextActivityAdapter extends ArrayAdapter<ResponseMessage> {
+
+    private TextView chatText;
+    private List<ResponseMessage> messageList = new ArrayList<ResponseMessage>(); // stores number of messages
+    private Context context; //context:어플리케이션에 대해서 현재 상태를 나타내는 역할
+
+    @Override
+    public void add(ResponseMessage object) {  // ArrayList에 아이템을 추가하는 코드입니다.
+        messageList.add(object);
+        super.add(object);
+    }
+
+    // Constructor - take lite
+    public CounselingTextActivityAdapter(Context context, int textViewResourceId) {
+        //context : 현재의 Context. 일반적으로 Adapter를 포함하는Activity의 instance가 들어간다.
+        //int textViewResourceId : TextView를 포함하는 layout 파일의 resource ID. 각 항목들을 어떤 형식으로 나타낼 것인지 결정해준다.
+        super(context, textViewResourceId);
+        this.context = context;
+    }
+
+    // returns total number of messages
+    public int getCount() {   //ListView에서 사용할 데이터의 총개수
+        return this.messageList.size();
+    }
+
+    // returns message at an index
+    public ResponseMessage getItem(int index) {
+        return this.messageList.get(index);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {   // Adapter 가 가지고 있는 data 를 어떻게 보여줄 것인가를 정의
+        //int position : 아이템의 인덱스를 의미하는 것으로 리스트뷰에서 보일 아이템의 위치 정보이다. 0부터 시작하여 아이템 개수만큼 파라미터로 전달된다.
+        //View convertView : 현재 인덱스에 해당하는 뷰 객체를 의미
+        //ViewGroup parent : 이 뷰를 포함하고 있는 부모 컨테이너 객체이다.
+        ResponseMessage messageObj = getItem(position);
+        View row = convertView; //ListView adapter에서 이전 뷰가 재사용이 가능할 경우, 그 뷰 정보.
+        LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //LayoutInvflater : xml에 정의된 Resource 를 View 객체로 반환해 주는 역할을 한다.
+        //getContext() : 현재 액티비티의 Context를 얻는다.
+        //getSystemService() : 시스템에서 제공하는 디바이스나 안드로이드 프레임워크내 기능을 다른 애플리케이션과 공유하고자 하는 시스템으로 부터 객체 얻을 때 사용
+        if (messageObj.isMe()) {
+            row = inflater.inflate(R.layout.counselingtextscreen_send, parent, false);
+            //inflater : xml로 정의된 view나 menu등을 실제 객체화 시키는 역할
+        } else {
+            row = inflater.inflate(R.layout.counselingtextscreen_receive, parent, false);
+        }
+        chatText = (TextView) row.findViewById(R.id.msgr);
+        chatText.setText(messageObj.getTextMessage());
+        return row;
+    }
+}

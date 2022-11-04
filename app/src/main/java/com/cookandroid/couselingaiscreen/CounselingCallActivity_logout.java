dc060d3fb46a23ab6c94f5b1a8f95e24a854fe37
임세진 <sejintong@naver.com>
2022-11-05 00:53:06 +0900

    비로그인 통화 기능 수정

--- app/src/main/java/com/cookandroid/couselingaiscreen/CounselingCallActivity_logout.java
+++ app/src/main/java/com/cookandroid/couselingaiscreen/CounselingCallActivity_logout.java
@@ -1,15 +1,50 @@
 package com.cookandroid.couselingaiscreen;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CounselingCallActivity_logout extends AppCompatActivity{
+    Intent intent;
+    SpeechRecognizer speechRecognizer;
+    TextToSpeech tts;
+    final int PERMISSION = 1;
+    CameraSurfaceView cameraSurfaceView;
+    String url = "http://192.168.219.106:5000/chat";
+
+    HashMap data = new HashMap();
+
+    RequestQueue mRequestQueue;
+
+    boolean recording = false;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.counselingcallscreen_logout);
@@ -17,6 +52,10 @@ public class CounselingCallActivity_logout extends AppCompatActivity{
             ImageButton recBtn = (ImageButton) findViewById(R.id.recBtn);
             ImageButton stopBtn = (ImageButton) findViewById(R.id.stopBtn);
             ImageButton stop_btn = (ImageButton) findViewById(R.id.stop_btn);
+
+            intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
 
 
         stop_btn.setOnClickListener(new View.OnClickListener(){
@@ -33,6 +72,7 @@ public class CounselingCallActivity_logout extends AppCompatActivity{
             public void onClick(View view) {
                 recBtn.setVisibility(View.INVISIBLE);
                 stopBtn.setVisibility(View.VISIBLE);
+                Toast.makeText(CounselingCallActivity_logout.this, "문장을 말씀하신 후 녹음정지 버튼을 눌러주세요.", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -45,5 +85,161 @@ public class CounselingCallActivity_logout extends AppCompatActivity{
         });
     }
 
+    public void postData(HashMap data) {
+        mRequestQueue = Volley.newRequestQueue(this);
 
+        JsonObjectRequest jsonobj = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(data),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            final String s = response.getString("message");
+                            tts.setPitch(1.0f);
+                            tts.setSpeechRate(1.0f);
+                            tts.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        jsonobj.setRetryPolicy(new com.android.volley.DefaultRetryPolicy(
+
+                20000 ,
+
+                com.android.volley.DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+
+                com.android.volley.DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        mRequestQueue.add(jsonobj);
+    }
+
+    RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+
+        }
+
+        @Override
+        public void onError(int i) {
+            String message;
+            switch (i) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    return;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    if(recording)
+                        StartRecord();
+                    return;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+            Toast.makeText(CounselingCallActivity_logout.this, "에러가 발생하였습니다. : " + message, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            String newText="";
+            for (int i = 0; i < matches.size(); i++) {
+                newText += matches.get(i);
+            }
+
+            data.put("message",newText);
+            postData(data);
+
+            //speechRecognizer.startListening(intent);
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
+    };
+
+    void StartRecord() {
+        recording = true;
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+        speechRecognizer.setRecognitionListener(listener);
+        speechRecognizer.startListening(intent);
+    }
+
+    void StopRecord() {
+        recording = false;
+
+        speechRecognizer.stopListening();
+    }
+
+    void CheckPermission() {
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED
+                    || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.INTERNET,
+                                Manifest.permission.RECORD_AUDIO}, PERMISSION);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+        super.onDestroy();
+    }
 }
